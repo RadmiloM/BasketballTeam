7edd12dfea68a7525eee7e157d8f3e13997e3921
RadmiloM <markovicradmilo35@gmail.com>
2023-10-29 15:55:34 +0100

    EnableTransactionManagement annotation added in BasketballApplication.

--- src/main/java/com/basketball/Basketball/BasketballApplication.java
+++ src/main/java/com/basketball/Basketball/BasketballApplication.java
@@ -3,9 +3,11 @@ package com.basketball.Basketball;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan({"com.basketball.Basketball"})
+@EnableTransactionManagement
 public class BasketballApplication {
 
     public static void main(String[] args) {
